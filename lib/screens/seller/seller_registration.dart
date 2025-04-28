0bab9dd0b3ac28184bca8a1d491e86f01478488a
0ced0 <151804456+0ced0@users.noreply.github.com>
2025-04-28 11:43:32 +0800

    with ongoing seller register

---
+++ lib/screens/seller/seller_registration.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class seller_registration extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Seller Page"
+      home: Scaffold (
+        body: Stack(
+          
+        )
+      )
+
+    )
+  }
+}
