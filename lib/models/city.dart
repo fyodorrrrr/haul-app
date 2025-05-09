521710ca3887e1a366113168e62d43955a69a074
Jeremy Garin <aquinojxse@gmail.com>
2025-05-09 23:57:20 +0800

    with API

---
+++ lib/models/city.dart
@@ -0,0 +1,11 @@
+class City {
+  final String name;
+
+  City({required this.name});
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      name: json['name'],
+    );
+  }
+}
